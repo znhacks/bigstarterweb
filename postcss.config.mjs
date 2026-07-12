c977ae66139b34dc1292adf3b5034fb2fe7d81b6
DAzzDev <raditdaffa375@gmail.com>
2026-07-12 23:53:03 +0700

    Update postcss.config.mjs

--- postcss.config.mjs
+++ postcss.config.mjs
@@ -1,5 +1,6 @@
-const config = {
-  plugins: ["@tailwindcss/postcss"]
+// postcss.config.mjs
+export default {
+  plugins: {
+    "@tailwindcss/postcss": {} // Menggunakan pemroses v4 baru
+  }
 };
-
-export default config;
